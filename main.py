c536ecc16ba8f7eb0fa94e24ef571f9b29bed267
hetao <yechaht@gmail.com>
2018-04-02 19:08:07 +0800

    实现获取资源ID

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from pyquery import PyQuery as pq
-from bs4 import BeautifulSoup
+from pyquery import PyQuery as pQuery
 import requests
+import re
 
-html = requests.get("http://www.qinzixuexi.com/ys_kw_p11.html").content
-# soup = BeautifulSoup(html, "lxml", from_encoding='utf-8')
-# print(soup.prettify())
-print(pq(html)('li.gxbox ul li.l1'))
+def download_mp3():
+    pass
+
+
+base_url = "http://www.qinzixuexi.com"
+# html = requests.get(base_url+"/ys_kw_p25.html").content
+# # print(pQuery(html)('li.gxbox ul li a'))
+# for link in pQuery(html)('li.gxbox ul li.l1 a'):
+#     print(link.attrib['href'] + " " + link.attrib['alt'])
+
+# html = requests.get(base_url+"/ys_kw/a20107.html").content
+# print(pQuery(html)('#vlink_1 ul li input'))
+# for rid in (pQuery(html)('#vlink_1 ul li input')):
+#     print(rid.attrib['value'])
+
+html = requests.get(base_url+"/player.php?rid=189090").content
+# {mp3:"http://p0124.iusheng.com/userUp/2018/03/23686/u07_lesson3_gt.mp3"}
+pattern = re.compile(r'\{mp3:".+"\}')
+match = pattern.match(html)
+if match:
+    print(match.group())
